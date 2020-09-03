34eceac86bea036e64e6483006cfab7ab91267c7
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2020-09-03 22:57:28 +0000

    for

---
+++ FunctionsFor.rs
@@ -0,0 +1,11 @@
+#![allow(unused)]
+fn main() {
+    println!("IREI IMPRIMIR MEU NOME 5 VEZES DUVIDA\n");
+    RepeteNome();
+}
+
+fn RepeteNome() {
+    for i in 0..5 {
+        println!("Victor");
+    }
+}
